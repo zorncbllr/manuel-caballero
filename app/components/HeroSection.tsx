c71cf19c7de7a8425dad3465c705faa1a875256e
Manuel <caballeromanuel203@gmail.com>
2026-08-18 04:41:56 +0800

    feat: updated hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -2,7 +2,18 @@ import MatrixAnimation from "./MatrixAnimation";
 
 function HeroSection() {
   return (
-    <div>
+    <div className="space-y-6">
+      <div className="-space-y-16">
+        <h1 className="text-[8rem]">Automate. Integrate.</h1>
+        <h1 className="text-[8rem]">Scale. </h1>
+      </div>
+      <div className="flex items-center gap-12 text-xl">
+        <span>AI Solutions</span>
+        <div className="p-1 bg-foreground rounded-full"></div>
+        <span>Workflow Automations</span>
+        <div className="p-1 bg-foreground rounded-full"></div>
+        <span>Scalable Software Systems</span>
+      </div>
       <MatrixAnimation height={1920 / 4} />
     </div>
   );
